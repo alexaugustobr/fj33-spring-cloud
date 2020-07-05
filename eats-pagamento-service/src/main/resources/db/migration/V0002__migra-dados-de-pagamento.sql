64554be8ecf8b30e044f0a719fef7ac9b5ef3fcc
alexaugustobr <alexcologia33@gmail.com>
2020-07-05 15:32:55 -0300

    FJ-33 - Cap03 - Migrados dados pagamento

---
+++ eats-pagamento-service/src/main/resources/db/migration/V0002__migra-dados-de-pagamento.sql
@@ -0,0 +1,3 @@
+INSERT INTO eats_pagamento.pagamento
+(id, valor, nome, numero, expiracao, codigo, status, forma_de_pagamento_id, pedido_id)
+VALUES(5, 51.80, 'ANDERSON DA SILVA', '1111 2222 3333 4444', '2022-07', '123', 'CRIADO', 2, 1);
