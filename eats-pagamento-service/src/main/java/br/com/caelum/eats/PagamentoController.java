e04ab57920a0961b1cfeaee004bbf3cb7797090f
alexaugustobr <alexcologia33@gmail.com>
2020-07-11 16:44:11 -0300

    FJ-33 - Cap05 - HAL

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/PagamentoController.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/PagamentoController.java
@@ -2,55 +2,112 @@ package br.com.caelum.eats;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/pagamentos")
+@RequestMapping(value = "/pagamentos", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @AllArgsConstructor
 @Slf4j
 class PagamentoController {
 
 	private PagamentoRepository pagamentos;
 	private PedidoRestClient pedidos;
-
+	
 	@GetMapping("/{id}")
-	PagamentoDto detalha(@PathVariable("id") Long id) {
-		Pagamento pagamento = pagamentos.findById(id).orElseThrow(() -> new ResourceNotFoundException());
-		return new PagamentoDto(pagamento);
+	public Resource<PagamentoDto> detalha(@PathVariable Long id) {
+		Pagamento pagamento = pagamentos.findById(id).orElseThrow(ResourceNotFoundException::new);
+		
+		List<Link> links = new ArrayList<>();
+		
+		Link self = linkTo(methodOn(PagamentoController.class).detalha(id)).withSelfRel();
+		links.add(self);
+		
+		if (Pagamento.Status.CRIADO.equals(pagamento.getStatus())) {
+			Link confirma = linkTo(methodOn(PagamentoController.class).confirma(id)).withRel("confirma");
+			links.add(confirma);
+			
+			Link cancela = linkTo(methodOn(PagamentoController.class).cancela(id)).withRel("cancela");
+			links.add(cancela);
+		}
+		
+		PagamentoDto dto = new PagamentoDto(pagamento);
+		Resource<PagamentoDto> resource = new Resource<>(dto, links);
+		
+		return resource;
 	}
-
+	
 	@PostMapping
-	ResponseEntity<PagamentoDto> cria(@RequestBody Pagamento pagamento, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<Resource<PagamentoDto>> cria(@RequestBody Pagamento pagamento,
+													   UriComponentsBuilder uriBuilder) {
 		pagamento.setStatus(Pagamento.Status.CRIADO);
 		Pagamento salvo = pagamentos.save(pagamento);
 		URI path = uriBuilder.path("/pagamentos/{id}").buildAndExpand(salvo.getId()).toUri();
-		return ResponseEntity.created(path).body(new PagamentoDto(salvo));
+		PagamentoDto dto = new PagamentoDto(salvo);
+		
+		Long id = salvo.getId();
+		
+		List<Link> links = new ArrayList<>();
+		
+		Link self = linkTo(methodOn(PagamentoController.class).detalha(id)).withSelfRel();
+		links.add(self);
+		
+		Link confirma = linkTo(methodOn(PagamentoController.class).confirma(id)).withRel("confirma");
+		links.add(confirma);
+		
+		Link cancela = linkTo(methodOn(PagamentoController.class).cancela(id)).withRel("cancela");
+		links.add(cancela);
+		
+		Resource<PagamentoDto> resource = new Resource<>(dto, links);
+		return ResponseEntity.created(path).body(resource);
 	}
-
+	
 	@PutMapping("/{id}")
-	PagamentoDto confirma(@PathVariable("id") Long id) {
-		Pagamento pagamento = pagamentos.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+	public Resource<PagamentoDto> confirma(@PathVariable Long id) {
+		Pagamento pagamento = pagamentos.findById(id).orElseThrow(ResourceNotFoundException::new);
 		pagamento.setStatus(Pagamento.Status.CONFIRMADO);
 		pagamentos.save(pagamento);
 		
 		Long pedidoId = pagamento.getPedidoId();
 		pedidos.avisaQueFoiPago(pedidoId);
 		
-		log.info(String.format("Pedido %s pago.", pedidoId));
+		List<Link> links = new ArrayList<>();
 		
-		return new PagamentoDto(pagamento);
+		Link self = linkTo(methodOn(PagamentoController.class).detalha(id)).withSelfRel();
+		links.add(self);
+		
+		PagamentoDto dto = new PagamentoDto(pagamento);
+		Resource<PagamentoDto> resource = new Resource<>(dto, links);
+		
+		return resource;
 	}
-
+	
 	@DeleteMapping("/{id}")
-	PagamentoDto cancela(@PathVariable("id") Long id) {
-		Pagamento pagamento = pagamentos.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+	public Resource<PagamentoDto> cancela(@PathVariable Long id) {
+		Pagamento pagamento = pagamentos.findById(id).orElseThrow(ResourceNotFoundException::new);
 		pagamento.setStatus(Pagamento.Status.CANCELADO);
 		pagamentos.save(pagamento);
-		return new PagamentoDto(pagamento);
+		
+		List<Link> links = new ArrayList<>();
+		
+		Link self = linkTo(methodOn(PagamentoController.class).detalha(id)).withSelfRel();
+		links.add(self);
+		
+		PagamentoDto dto = new PagamentoDto(pagamento);
+		Resource<PagamentoDto> resource = new Resource<>(dto, links);
+		
+		return resource;
 	}
 
 }
