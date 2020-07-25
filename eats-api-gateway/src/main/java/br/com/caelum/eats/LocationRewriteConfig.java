48c2fe833d15a6e5029b8a7f3d5f7323e0272b47
alexaugustobr <alexcologia33@gmail.com>
2020-07-25 16:35:34 -0300

    FJ-33 - Cap06 - Implementado LocationRewriteConfig

---
+++ eats-api-gateway/src/main/java/br/com/caelum/eats/LocationRewriteConfig.java
@@ -0,0 +1,24 @@
+package br.com.caelum.eats;
+
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+class LocationRewriteConfig {
+	
+	@Bean
+	LocationRewriteFilter locationRewriteFilter() {
+		return new LocationRewriteFilter() {
+			@Override
+			public boolean shouldFilter() {
+				int statusCode = RequestContext.getCurrentContext().getResponseStatusCode();
+				return HttpStatus.valueOf(statusCode).is3xxRedirection() ||
+						HttpStatus.valueOf(statusCode).is2xxSuccessful();
+			}
+		};
+	}
+	
+}
